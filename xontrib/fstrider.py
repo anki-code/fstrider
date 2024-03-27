a541424b24ee7ecae1d68fb65baa32158b0b9842
a <1@1.1>
2024-03-27 11:31:03 +0100

    update

--- xontrib/fstrider.py
+++ xontrib/fstrider.py
@@ -3,7 +3,7 @@
 import fstrider as _fstrider
 from shutil import which as _which
 
-_cmd = "fstrider" if _which("fs") else "fs"
+_cmd = __xonsh__.env.get('XONTRIB_FSTRIDER_ALIAS', "fstrider" if _which("fs") else "fs")
 
 @aliases.register(_cmd)
 def _alias_strider(args):
